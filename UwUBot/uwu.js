d6b0212a6f91ca93e6e06aba136fbfbc447fc84d
Andy Mahoney <32443674+BoneKing@users.noreply.github.com>
2018-11-19 17:09:24 -0500

    now waits 5sec

--- UwUBot/uwu.js
+++ UwUBot/uwu.js
@@ -26,37 +26,46 @@ bot.on("message",msg => {
 		if (msg.content.includes(" is ") || msg.content.includes("'s ")){
 		//	msg.react('UwU')
 			msg.channel.send("UwU what's this? "+msg.author);
+			setTimeout(5000);
 			//if(counter == 0){ counter = randint(10)+5;}
 		}
 		else if(msg.content.includes("UwU Bot") || msg.content.includes(" UwU Bot ")){
                         msg.channel.send("*Is nervous* H-Hewwo UwU");
+			setTimeout(5000);
                 }
 		else if(msg.content.includes(" anime ") || msg.content.includes(" anime's ")){
 		//	msg.react('UwU')
 			msg.channel.send("OwO what's this? "+msg.author);
+			setTimeout(5000);
 			//if(counter == 0){ counter = randint(10)+5;}
 		}
 		else if(msg.content.includes(" UwU ") || msg.content.includes("UwU")){
 	//		msg.react('UwU')
 			msg.channel.send("Uwufu desu "+msg.author);
+			setTimeout(5000);
 			//if(counter == 0){ counter = randint(10)+5;}	
 		}
 		else if(msg.content.includes("Fuck") || msg.content.includes(" fuck ") || msg.content.includes(" fucking ") || msg.content.includes("Fucking") || msg.content.includes("fuck")){
 			msg.channel.send("Oopsie woopsie, looks like we made a little fuckey wuckey, a little fucko boingo UwU");
+			setTimeout(5000);
 			//if(counter == 0){counter = randint(10)+5;}
 		}
         	else if(msg.content.includes(" woops ") || msg.content.includes("Woops") || msg.content.includes("woops") || msg.content.includes("whoops") || msg.content.includes("Whoops")){
             		msg.channel.send("Oopsie woopsie UwU! It UwU looks like UwU I've dropped UwU some UwUs all over the UwU place UwU");
+			setTimeout(5000);
        		}
 		else if(msg.content.includes("Hey")|| msg.content.includes("hey")){
 			msg.channel.send("Pwease give me huggie wuggies "+msg.author);
 			msg.channel.send("UwU");
+			setTimeout(5000);
 		}
 		else if(msg.content.includes("Cute") || msg.content.includes("cute") || msg.content.includes(" cute ")){
 			msg.channel.send("*Pounces on you* OwO What's this? *Notices your bulge*");
+			setTimeout(5000);
 		}
 		else if(msg.author.username === "Isabelle"){
 			msg.channel.send("H-Hewwo IsaBewwe UwU");
+			setTimeout(5000);
 		}
 		
 	} 
