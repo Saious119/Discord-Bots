c1a25ded94acfed0b644ca8a662fb49fd0058116
Andy Mahoney <32443674+BoneKing@users.noreply.github.com>
2020-09-20 13:33:17 -0400

    untested faster image sending

--- UwUBot/uwu.js
+++ UwUBot/uwu.js
@@ -188,25 +188,38 @@ bot.on("message",msg => {
 			msg.channel.send("pwease wook in nsf-doub-UwU");
 			msg.client.channels.get("486580756966277120").send("give me a couple minutes to search 4chan");
 			//image();
-			exec('./getImage.sh', (err, stdout, stderr) => {
-				if (err) {
-				  console.error(`exec error: ${err}`);
-				  return;
-				}
-			  
-				console.log(`Number of files ${stdout}`);
-			});
-			sleep(240*1000);
 			const dirs = fs.readdirSync('downloads');
+			if(dirs.length < 2){
+				msg.client.channels.get("486580756966277120").send("Outta images UwU, gowin' to tha stowe");
+				exec('./getImage.sh', (err, stdout, stderr) => {
+					if (err) {
+				  	console.error(`exec error: ${err}`);
+				  	return;
+					}
+			  
+					console.log(`Number of files ${stdout}`);
+				});
+				sleep(240*1000);
+			}
+			//const dirs = fs.readdirSync('downloads');
 			var fileIndex = randint(dirs.length-1);
 			var imgFile = dirs[fileIndex];
 			var imgloc = './downloads/'+imgFile;
 			//msg.channel.send(imgloc);
 			msg.client.channels.get("486580756966277120").send("I found something", {files: [imgloc]});
 			//let nsfw = client.channels.cache.find(c => c.name === 'nsfw');
-			msg.channel.send("give me a couple minutes to search 4chan");
+			//msg.channel.send("give me a couple minutes to search 4chan");
 		
 			sleep(60*1000);	
+			exec('rm -rf downloads/'+imgFile, (err, stdout, stderr) => {
+				if (err) {
+					console.error(`exec error: ${err}`);
+					return;
+				  }
+				
+				  console.log(`Number of files ${stdout}`);
+			});
+			/*
 			exec('rm -r downloads/', (err, stdout, stderr) => {
 				if (err) {
 				  console.error(`exec error: ${err}`);
@@ -215,8 +228,9 @@ bot.on("message",msg => {
 			  
 				console.log(`Number of files ${stdout}`);
 			});
+			*/
 			
-			msg.channel.send("I sent an image, probably");
+			msg.channel.send("I sent an image, pwobably UwU");
 		}
 		else {
 			counter--;
