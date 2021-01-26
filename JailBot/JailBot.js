f6673743dc09fa66170e3752b6007f396b1d8e8b
Andy Mahoney <32443674+BoneKing@users.noreply.github.com>
2021-01-25 22:14:23 -0500

    JailBot added

---
+++ JailBot/JailBot.js
@@ -0,0 +1,42 @@
+var Discord = require("discord.js");
+var logger = require("winston");
+var auth = require("./auth.json");
+var sleep = require('system-sleep');
+
+
+//Logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+  colorize : true
+});
+logger.level = "debug";
+//Robot time
+var bot = new Discord.Client();
+//var client = new Discord.Client();
+var counter = 0;
+var isReady = true;
+let cringerole = message.guild.roles.cache.find(role => role.name == "Cringe");
+bot.on("ready",() => {
+  logger.info("Connected");
+});
+
+bot.on("message",msg => {	
+	if(msg.author == bot.user){
+		//react to all message here
+	}	
+	else{
+		if(msg.content.includes("UwU") || msg.content.includes("OwO") || msg.content.includes("UwUBot") || msg.content.includes("image") || msg.content.includes("nuggs") || msg.content.includes("ASMR")){
+			msg.author.roles.add(cringerole);
+			msg.channel.send("You have interacted with the Cringe UwUBot, you are now Cringe for 1 minute");
+			sleep(60*1000);
+			msg.author.roles.remove(cringerole);
+			msg.channel.send(msg.author + " has been freed from the prison that is cringe, I wish you a sucessful rehabilitation back into society");
+    	}
+  	}
+});
+
+function randint(bound) {
+	return Math.round(Math.random()*bound);
+}
+
+bot.login(auth.token)
---
+++ JailBot/runJailBot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while true 
+do
+	node JailBot.js
+done
