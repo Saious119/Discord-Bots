97234fadb0170197db155b346f00b0899aae0018
Andy Mahoney <32443674+BoneKing@users.noreply.github.com>
2021-01-26 11:36:42 -0500

    switch has to find

--- JailBot/JailBot.js
+++ JailBot/JailBot.js
@@ -28,7 +28,7 @@ bot.on("message",msg => {
 		if(msg.content.includes("UwU") || msg.content.includes("OwO") || msg.content.includes("UwUBot") || msg.content.includes("image") || msg.content.includes("nuggs") || msg.content.includes("ASMR")){
 			let cringerole = msg.guild.roles.get("793708658345246730");
 			let member = msg.member;
-			if(member.roles.has(cringerole)) {
+			if(member.roles.find(r => r.name === "Cringe")) {
 				console.log("Silence inmate!");
 			} else {
 				msg.channel.send(msg.author+"");
