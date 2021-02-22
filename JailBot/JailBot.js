80d695370b9f2b6609f3717f1114f4776a1dac49
Andy Mahoney <32443674+BoneKing@users.noreply.github.com>
2021-02-22 15:23:09 -0500

    fixing remove role on JailBot

--- JailBot/JailBot.js
+++ JailBot/JailBot.js
@@ -39,7 +39,7 @@ bot.on("message",msg => {
 				member.addRole(cringerole).catch(console.error);
 				msg.channel.send("You have interacted with the Cringe UwUBot, you are now Cringe for 1 minute");
 				sleep(60*1000);
-				member.removeRoles(cringerole).catch(console.error);
+				member.roles.remove(cringerole).catch(console.error);
 				sleep(5*1000);
 				msg.channel.send(msg.author + " has been freed from the prison that is cringe, I wish you a sucessful rehabilitation back into society");
 				sleep(5*1000);
