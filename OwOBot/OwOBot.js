82c886926310f6c911c8f33222bc7afd9b3a31f8
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2021-07-10 16:49:58 -0400

    reorganized OwOBot

--- OwOBot/OwOBot.js
+++ OwOBot/OwOBot.js
@@ -27,59 +27,65 @@ bot.on("ready",() => {
 });
 bot.on("message",msg => {	
     if(msg.content.includes("OwO, what's this?") || msg.content.includes("Θωθ, what's this?")){ //trigger
-	console.log("OwO Triggered");
-    var msgSplit = msg.content.split("? ", 2); //[0] = trigger [1] = query
-	//const messages = msg.channel.messages.fetch({ limit: 2 });
-	//const lastMessage = messages.last();
-	if(msgSplit[1] == "" || msgSplit[1] == " "){
-		exit(1);
-	}
-	//msgSplit.split(/" "/(.+));
-	console.log("request is: ");
-	console.log(msgSplit[1]);
-        var params = { //sent to api
-            action: "query",
-            list: "search",
-            srsearch: msgSplit[1],
-            format: "json"
-        };
-        url = url + "?origin=*";
-        Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-
-        fetch(url)
-            .then(function(response){return response.json();})
-            .then(function(response) {
-                if (response.query.search[0].title === msgSplit[1]){ //if there is a page match
-                    console.log("Your search page exists on English Wikipedia" );
-		            msg.channel.send("Yes, I know of this topic, here:");
-		            msg.channel.send("https://wikipedia.org/wiki/"+response.query.search[0].title.replace(" ","_"));
-                }
-                else {
-                    console.log("Your search page DOES NOT exists on English Wikipedia" );
-                    var UrbanData = UrbanDicSearch(msgSplit[1]);
-		    sleep(2000);
-                    console.log("UrbanData = "+UrbanData);
-                    if(UrbanData != null){
-                        var UrbanDef = UrbanData.replace("[", "");
-                        UrbanDef = UrbanDef.replace("]", "");
-                        msg.channel.send(msgSplit[1]+": "+UrbanDef);
-                    }
-                    else{
-                        console.log("Your search page DOES NOT exists on English Wikipedia or Urban Dictionary" );
-                        msg.channel.send("Hmmmm, Θωθ does not know of this.");
-                    }
-                }
-            })
-            .catch(function(error){console.log(error);});
+	    console.log("OwO Triggered");
+        var msgSplit = msg.content.split("? ", 2); //[0] = trigger [1] = query
+	    //const messages = msg.channel.messages.fetch({ limit: 2 });
+	    //const lastMessage = messages.last();
+	    if(msgSplit[1] == "" || msgSplit[1] == " "){
+		    exit(1);
+	    }
+	    console.log("request is: "+msgSplit[1]);
+        var WikiData = WikiSearch(msgSplit[1]);
+        if(WikiData != null){
+            msg.channel.send("Yes, I know of this topic, here:");
+            msg.channel.send(WikiData);
+            exit(0);
+        }    
+        var UrbanData = UrbanDicSearch(msgSplit[1]);
+	    sleep(2000);
+        if(UrbanData != null){
+            msg.channel.send("Yes, I know of this topic, here:");
+            msg.channel.send(UrbanData);
+            exit(0);
+        }
+        console.log("Your search page DOES NOT exists on English Wikipedia or Urban Dictionary" );
+        msg.channel.send("Hmmmm, Θωθ does not know of this.");
     }
 });
+
+function WikiSearch(searchTerm){
+    var returnString = null;
+    var params = { //sent to api
+        action: "query",
+        list: "search",
+        srsearch: searchTerm,
+        format: "json"
+    };
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            if (response.query.search[0].title === msgSplit[1]){ //if there is a page match
+                console.log("Your search page exists on English Wikipedia" );
+                //msg.channel.send("Yes, I know of this topic, here:");
+                returnString = "https://wikipedia.org/wiki/"+response.query.search[0].title.replace(" ","_");
+            }
+            else {
+                console.log("Your search page DOES NOT exists on English Wikipedia" );
+            }
+        })
+        .catch(function(error){console.log(error);});
+    return returnString;
+}
 
 function UrbanDicSearch(searchTerm){
     console.log("in func");
     var def = null; 
     var data = ud.define(searchTerm, (error, results) => {
         console.log("HERE");
-	if (error) {
+	    if (error) {
           console.error(`define (callback) error - ${error.message}`)
           return null;
         }
@@ -87,16 +93,21 @@ function UrbanDicSearch(searchTerm){
         console.log('define (callback)')
       
         Object.entries(results[0]).forEach(([key, prop]) => {
-          console.log(`${key}: ${prop}`)
-          if(key == "definition"){
-	      console.log("prop = "+prop);
-	      def = prop;
-          }
+            console.log(`${key}: ${prop}`)
+            if(key == "definition"){
+	            console.log("prop = "+prop);
+	            def = prop;
+            }
         })
-	//sleep(15000);
-      })
+	    //sleep(15000);
+    })
     console.log(data);
     console.log("def = "+def);
+    if(def != null){
+        def.replace("[", "");
+        def.replace("]", "");
+        def = msgSplit[1]+": "+def;
+    }
     return def;
 }
 
