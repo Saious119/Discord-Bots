79d62edf1086ea94506381c4eaf9f6fffd54420f
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2021-09-01 22:29:01 -0400

    maybe JailBot works

--- JailBot/JailBot.js
+++ JailBot/JailBot.js
@@ -20,27 +20,28 @@ bot.on("ready",() => {
   logger.info("Connected");
 });
 
-bot.on("message",msg => {	
+bot.on("message", msg => {	
 	if(msg.author == bot.user){
 		//react to all message here
 	}	
 	else{
 		if(msg.content.includes("UwU") || msg.content.includes("OwO") || msg.content.includes("UwUBot") || msg.content.includes("image") || msg.content.includes("nuggs") || msg.content.includes("ASMR")){
-			let cringerole = msg.guild.roles.get("793708658345246730");
+			//let cringerole = msg.guild.roles.get("793708658345246730");
+			let cringerole = await msg.guild.roles.cache.find(r => r.name === "Cringe");
 			console.log("@ing user");
 			msg.channel.send(msg.author+"");
 			let usera = msg.author;
 			msg.channel.send("Cringe Detected:");
 			let member = msg.member;
-			if(msg.member.roles.has("793708658345246730")) {
+			if(msg.member.roles.has(r => r.name === "Cringe")) {
 				console.log("Silence inmate!");
 			} else {
 				sleep(5*1000);
 				//let member = msg.member;
-				msg.member.addRole("793708658345246730").catch(console.error);
+				msg.member.addRole(cringerole).catch(console.error);
 				msg.channel.send("You have interacted with the Cringe UwUBot, you are now Cringe for 1 minute");
 				sleep(60*1000);
-				msg.member.roles.remove("793708658345246730").catch(console.error);
+				msg.member.roles.removeRole(cringerole).catch(console.error);
 				sleep(5*1000);
 				msg.channel.send(msg.author + " has been freed from the prison that is cringe, I wish you a sucessful rehabilitation back into society");
 				sleep(5*1000);
