e8bf51a23f557a72f46e53d6d48e1f761bd2ae22
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2021-09-17 21:17:44 -0400

    I looked at docs

--- UwUBot/uwu.js
+++ UwUBot/uwu.js
@@ -226,8 +226,8 @@ bot.on("message", async msg => {
 				}
 				if(!found){
 					console.log("removing");
-					msg.delete(1000); //Supposed to delete message
-      				msg.channel.send(msg.content.slice(5, msg.content.length));
+					msg.delete.then(msg => console.log(`Deleted message from ${msg.author.username}`)).catch(console.error); //Supposed to delete message
+      				//msg.channel.send(msg.content.slice(5, msg.content.length));
 					console.log("removed");
 				}
 			}
