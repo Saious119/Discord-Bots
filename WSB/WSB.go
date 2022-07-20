dfa5bfd34ff105e5dd1b29eab0efbabfa211f4a0
Benjamin Lannon <benjamin@lannonbr.com>
2022-07-20 17:23:39 -0400

    Clean up WSB

--- WSB/WSB.go
+++ WSB/WSB.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"bufio"
+	"io/ioutil"
 	"math/rand"
 	"os"
 	"os/signal"
@@ -17,61 +18,12 @@ import (
 const LEN int = 100
 const MAX_CHAIN int = 8192
 
-var lyrics []string
+var corpus []string
 var chain []MarkovNode
-var buff []byte
 
-func PirateBrain(s *discordgo.Session, m *discordgo.MessageCreate) {
+func onMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
-	}
-	if strings.Contains(m.Author.String(), "VimBot") {
-		return
-	}
-
-	//NSFW and spam
-	if m.ChannelID == "486580756966277120" || m.ChannelID == "486580615425032192" {
-		return
-	}
-
-	for i := 0; i < len(m.Mentions); i++ {
-		if strings.Contains(m.Mentions[i].String(), "GamerQuote") || strings.Contains(m.Mentions[i].String(), "everyone") {
-			if strings.Contains(m.Author.String(), "VimBot") {
-				continue
-			}
-			if strings.Contains(m.Author.String(), "PingBot") {
-				var responce string = "BC:"
-				if m.Content[:3] == "BR:" {
-					sin := bufio.NewScanner(strings.NewReader(m.Content))
-					sin.Split(bufio.ScanRunes)
-					foundBegin := false
-					for sin.Scan() {
-						if !foundBegin {
-							if sin.Text() == ":" {
-								foundBegin = true
-								continue
-							}
-						}
-						if foundBegin {
-							if sin.Text() == ":" {
-								break
-							}
-							responce += sin.Text()
-						}
-					}
-					responce += ":PONG 0000000000"
-				}
-				s.ChannelMessageSend(m.ChannelID, responce)
-				return
-			}
-			if m.ChannelID != "486580637139075082" { // SPAM Channel
-				markov := markov(chain, LEN)
-				s.ChannelMessageSend(m.ChannelID, markov)
-			} else {
-				markov := markov(chain, LEN)
-				s.ChannelMessageSend(m.ChannelID, markov)
-			}
-		}
 	}
 
 	if strings.Contains(strings.ToLower(m.Content), "gamer") || strings.Contains(strings.ToLower(m.Content), "quote") {
@@ -79,24 +31,24 @@ func PirateBrain(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 		s.ChannelMessageSend(m.ChannelID, markov)
 	}
-
 }
 
 func main() {
 	rand.Seed(int64(time.Now().Nanosecond()))
-	//	auth, err := ioutil.ReadFile("auth.txt")
-	//	if err != nil {
-	//		panic(err)
-	//	}
 
-	// Convert []byte to string and print to screen
-	TOKEN := "OTY5Mzk5OTQ0ODQ0ODA0MTE2.Yms2DQ.BZUazQ-bktPlg8yT7K4V5Ecm3eY"
+	authBuff, err := ioutil.ReadFile("auth.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	TOKEN := string(authBuff)
+
 	ds, err := discordgo.New("Bot " + TOKEN)
 	if err != nil {
 		panic(err)
 	}
 
-	ds.AddHandler(PirateBrain)
+	ds.AddHandler(onMessage)
 	err = ds.Open()
 	if err != nil {
 		panic(err)
@@ -112,11 +64,11 @@ func main() {
 	in.Split(bufio.ScanWords)
 
 	for in.Scan() {
-		lyrics = append(lyrics, in.Text())
+		corpus = append(corpus, in.Text())
 	}
-	chain = createChain(lyrics)
+	chain = createChain(corpus)
 
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 }
--- WSB/markov.go
+++ WSB/markov.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"math/rand"
-	"strings"
 )
 
 type MarkovNode struct {
 	value string
 	next  []string
-}
-
-func genChain(frozen string) []MarkovNode {
-	sin := bufio.NewScanner(strings.NewReader(frozen))
-	sin.Split(bufio.ScanWords)
-	var inputTokens []string
-	for sin.Scan() {
-		inputTokens = append(inputTokens, sin.Text())
-	}
-	chain := createChain(inputTokens)
-	return chain
 }
 
 func getNode(chain []MarkovNode, key string) MarkovNode {
@@ -42,22 +29,6 @@ func isIn(chain []MarkovNode, node MarkovNode) (bool, int) {
 
 func createChain(input []string) []MarkovNode {
 	var chain []MarkovNode
-	for i := 0; i < len(input); i++ {
-		node := getNode(chain, input[i])
-		if i+1 < len(input) {
-			node.next = append(node.next, input[i+1])
-		}
-		in, i := isIn(chain, node)
-		if !in {
-			chain = append(chain, node)
-		} else {
-			chain[i] = node
-		}
-	}
-	return chain
-}
-
-func addToChain(chain []MarkovNode, input []string) []MarkovNode {
 	for i := 0; i < len(input); i++ {
 		node := getNode(chain, input[i])
 		if i+1 < len(input) {
