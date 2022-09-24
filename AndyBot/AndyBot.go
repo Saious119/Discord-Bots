3ba15efcc8af5d31ed7bc5d3de0b50ccc5922076
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2022-09-23 20:57:16 -0400

    Andy Bot

---
+++ AndyBot/AndyBot.go
@@ -0,0 +1,74 @@
+package main
+
+//This code is from bashawhm's github page for his AutoDolly Bot, all credit goes to him
+
+import (
+	"bufio"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const LEN int = 100
+const MAX_CHAIN int = 8192
+
+var corpus []string
+var chain []MarkovNode
+
+func onMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.Contains(strings.ToLower(m.Content), "andy") || strings.Contains(strings.ToLower(m.Content), "Andy") {
+		markov := markov(chain, rand.Intn(LEN))
+
+		s.ChannelMessageSend(m.ChannelID, markov)
+	}
+}
+
+func main() {
+	rand.Seed(int64(time.Now().Nanosecond()))
+
+	authBuff, err := ioutil.ReadFile("auth.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	TOKEN := strings.TrimSuffix(string(authBuff), "\n")
+
+	ds, err := discordgo.New("Bot " + TOKEN)
+	if err != nil {
+		panic(err)
+	}
+
+	ds.AddHandler(onMessage)
+	err = ds.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Open("AndyQuotes.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	in := bufio.NewScanner(bufio.NewReader(file))
+	in.Split(bufio.ScanWords)
+
+	for in.Scan() {
+		corpus = append(corpus, in.Text())
+	}
+	chain = createChain(corpus)
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+}
