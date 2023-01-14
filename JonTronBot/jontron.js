febda29791f3551d1874024f8dec9498f71c316e
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2023-01-14 16:30:10 -0500

    updating jontron and terry davis for new discord.js

--- JonTronBot/jontron.js
+++ JonTronBot/jontron.js
@@ -2,6 +2,8 @@ var Discord = require("discord.js");
 var logger = require("winston");
 var auth = require("./auth.json");
 
+const { Client, Message, VoiceChannel, GatewayIntentBits, ChannelType, ApplicationCommandOptionType, InteractionType } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent] });
 //Logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -9,15 +11,14 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = "debug";
 //Robot time
-var bot = new Discord.Client();
 var quotes = ["I'm a brave boy .... NOT A BRAVE ENOUGH BOY FOR THIS!", "Now we're getting into some deep shit I don't know if I'm ready for that, also WHAT WHAT WHAT WHAAT!", "*Blows Whistle* Rape!", "Slap it on with the might of Zeus", "Paula Dean is the reason all of my friends are dead!", "WHAT!? WHAT THE FUCK!?", "Man, 1910 times were scaaaaary!", "Cars...? Cars?! CAAAAAAARS!!!!! AND IIIIIIIII—HOLY SHIT!—WILL ALWAYS LOVE YOUICAN'TBELIEVEYOUDIDTHISTOMEGODDAMMITHOWCOULDYOUDOTHISTOME?!", "Good one, Bubsy! Hey, wanna be a cast member on Sat-purr-day Night Live? I know you'll make the MEOWST OF IT! [Suddenly distant] I'm leavin' ya, Bubsy!", "Good God, China! All about symbols! Couldn't even make the alphabet!", "Oh, you got me Monopoly this year? For the Nintendo 64? Well, this would have been great back in 1864. Y'know, when it was impressive just to not die from being 35.", "Of course, don't forget everyone's favourite crime-fighting alliance, Sense of Right! Everyone's here: Batman, Superman, Shhhrek, a caarrr…ooohh nooo…", "9 is the maximum life you can get.", "I DON'T EVEN CARE.....I care immenseley"];
 var len = quotes.length;
 //var counter = 0;
 
-bot.on("ready",() => {
+client.on("ready",() => {
   logger.info("Connected");
 });
-bot.on("message",msg => {	
+client.on("message",msg => {	
 	if(msg.author == bot.user){
 		//future work maybe?
 	}
@@ -34,4 +35,4 @@ function getRandomInt(max) {
 }
 
 
-bot.login(auth.token)
+client.login(auth.token)
--- TerryDavisBot/terrydavisbot.js
+++ TerryDavisBot/terrydavisbot.js
@@ -2,6 +2,8 @@ var Discord = require("discord.js");
 var logger = require("winston");
 var auth = require("./auth.json");
 
+const { Client, Message, VoiceChannel, GatewayIntentBits, ChannelType, ApplicationCommandOptionType, InteractionType } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.MessageContent] });
 //Logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -9,16 +11,15 @@ logger.add(new logger.transports.Console, {
 });
 logger.level = "debug";
 //Robot time
-var bot = new Discord.Client();
 //WARNING: SOME CONTENT IS OFFENSIVE, NO CONTRIBUTOR SUPPORTS TERRY DAVIS' POLITICAL VIEWS (we do find it funny and iconic to his character though)
 var quotes = ["I'm the best programmer to ever live, I'm better than anyone else before me.","It's either Niggerlisious or Divine Intellect","7-bit signged ASCII is retarded","N*$@!rlishous","Yeah, I killed a CIA n%#$%! with my car in 1999. Score one for the good guys.", "The CIA has a seven-year-old deepthoating a 45 at DMV next to me fucking with me. I'll teach him to pull the trigger", "What color should I choose? Let's see my floors are brown...God's favorite color is cyan.", "Proffesonals write their own Compilers", "Go download my 2MB Distro at http://www.templeos.org/ now on http://mirror.clarkson.edu/templeos/ !!!", "Why can you do it? Cause fuck yeah that's why!", "It's those fuckin' glow in the dark C.I.A N$@!&%s"];
 var len = quotes.length;
 //var counter = 0;
 
-bot.on("ready",() => {
+client.on("ready",() => {
   logger.info("Connected");
 });
-bot.on("message",msg => {	
+client.on("message",msg => {	
 	if(msg.author == bot.user){
 		//future work maybe?
 	}
@@ -35,4 +36,4 @@ function getRandomInt(max) {
 }
 
 
-bot.login(auth.token)
+client.login(auth.token)
