e3c0e52747d2db648fb20d1dd9be1f7029fb5436
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2024-10-16 19:57:40 -0400

    exception handling is good

--- ScribeBot/scribe_bot.py
+++ ScribeBot/scribe_bot.py
@@ -15,49 +15,57 @@ token_file = open("auth.txt", 'r', encoding="utf-8")
 token = token_file.read()
 
 def write_wsb_quotes(quotes):
-    if os.path.exists("GamerQuotes.txt"):
-        os.remove("GamerQuotes.txt")
-    file = open("GamerQuotes.txt", "a+", encoding="utf-8")
-    for quote in quotes:
-        file.writelines(quote+'\n')
-    print("WSB Quotes Written to File!")
-    response = requests.get("http://localhost:8081/refreshQuotes") #Call WSB's API to reload the quotes file
-    print("Called WSB's API")
+    try:
+        if os.path.exists("GamerQuotes.txt"):
+            os.remove("GamerQuotes.txt")
+        file = open("GamerQuotes.txt", "a+", encoding="utf-8")
+        for quote in quotes:
+            file.writelines(quote+'\n')
+        print("WSB Quotes Written to File!")
+        response = requests.get("http://localhost:8081/refreshQuotes") #Call WSB's API to reload the quotes file
+        print("Called WSB's API")
+    except Exception as e:
+        print(e)
 
 def write_andy_quotes(quotes):
-    if os.path.exists("AndyQuotes.txt"):
-        os.remove("AndyQuotes.txt")
-    file = open("AndyQuotes.txt", "a+", encoding="utf-8")
-    for quote in quotes:
-        file.write(quote+'\n')
-    print("Andy Quotes Written to File!")
-    response = requests.get("http://localhost:8080/refreshQuotes") #Call AndyBot's API to reload the quotes file
-    print("Called AndyBot's API")
+    try:
+        if os.path.exists("AndyQuotes.txt"):
+            os.remove("AndyQuotes.txt")
+        file = open("AndyQuotes.txt", "a+", encoding="utf-8")
+        for quote in quotes:
+            file.write(quote+'\n')
+        print("Andy Quotes Written to File!")
+        response = requests.get("http://localhost:8080/refreshQuotes") #Call AndyBot's API to reload the quotes file
+        print("Called AndyBot's API")
+    except Exception as e:
+        print(e)
 
 async def get_history_of_quotes_channel(channel):
     print("Getting Quotes")
-    messages = [message async for message in channel.history(limit=None)] # messages is now a list of Message.. #None lets us retrieve everything
-    wsb_quotes= []
-    andy_quotes = []
-    for message in messages:
-        #please stop deadnaming my friend :(
-        message.content = message.content.replace("ben", "mel")
-        message.content = message.content.replace("Ben", "Mel")
-        message.content = message.content.replace("BEN", "MEL")
-        #do stuff
-        if '/' and ':' not in message.content: #filter out time stamps
-            message_to_add = message.content
-            if " - " in message.content: #filter out quote attributions 
-                message_to_add = message.content.split(" - ")[0]
-            if " ~ " in message.content:
-                message_to_add = message.content.split(" ~ ")[0]
-            if '357280188025012252' in message.content: #Check for AndyBot Quote
-                andy_quotes.append(message_to_add)
-            wsb_quotes.append(message_to_add)
-
-    write_wsb_quotes(wsb_quotes)
-    write_andy_quotes(andy_quotes)
-    print("Got Quotes")
+    try:
+        messages = [message async for message in channel.history(limit=None)] # messages is now a list of Message.. #None lets us retrieve everything
+        wsb_quotes= []
+        andy_quotes = []
+        for message in messages:
+            #please stop deadnaming my friend :(
+            message.content = message.content.replace("ben", "mel")
+            message.content = message.content.replace("Ben", "Mel")
+            message.content = message.content.replace("BEN", "MEL")
+            #do stuff
+            if '/' and ':' not in message.content: #filter out time stamps
+                message_to_add = message.content
+                if " - " in message.content: #filter out quote attributions 
+                    message_to_add = message.content.split(" - ")[0]
+                if " ~ " in message.content:
+                    message_to_add = message.content.split(" ~ ")[0]
+                if '357280188025012252' in message.content: #Check for AndyBot Quote
+                    andy_quotes.append(message_to_add)
+                wsb_quotes.append(message_to_add)
+        write_wsb_quotes(wsb_quotes)
+        write_andy_quotes(andy_quotes)
+        print("Got Quotes")
+    except Exception as e:
+        print(e)
 
 @client.event
 async def on_ready():
